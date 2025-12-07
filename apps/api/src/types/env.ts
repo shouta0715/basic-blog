1e85c48ab2b271591e14dcfce6d4817cfa5d90d5
shouta0715 <kshouta0715@gmail.com>
2025-12-07 14:56:49 +0900

    chore: env types

--- apps/api/src/types/env.ts
+++ apps/api/src/types/env.ts
@@ -1,3 +1,9 @@
+import { auth } from "@/lib/auth";
+type Variables = {
+  user: typeof auth.$Infer.Session.user;
+  session: typeof auth.$Infer.Session.session;
+} & Cloudflare.Env;
+
 export type Env = {
-  Variables: Cloudflare.Env;
+  Variables: Variables;
 };
