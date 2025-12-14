121181de384422773f8b5dc119f4de6af44f1920
shouta0715 <kshouta0715@gmail.com>
2025-12-14 09:31:27 +0900

    chore: uniwindのsafeareaのセットアップ

--- apps/native/src/contexts/providers.tsx
+++ apps/native/src/contexts/providers.tsx
@@ -1,5 +1,13 @@
+import { SafeAreaListener } from "react-native-safe-area-context";
+import { Uniwind } from "uniwind";
 import { SplashScreenController } from "./splash-screen-controller";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => (
-  <SplashScreenController>{children}</SplashScreenController>
+  <SafeAreaListener
+    onChange={({ insets }) => {
+      Uniwind.updateInsets(insets);
+    }}
+  >
+    <SplashScreenController>{children}</SplashScreenController>
+  </SafeAreaListener>
 );
