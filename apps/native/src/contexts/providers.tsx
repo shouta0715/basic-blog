f8f3572fa142a09294a81123b7f7f589e8b58feb
shouta0715 <kshouta0715@gmail.com>
2025-12-20 10:36:28 +0900

    feat: tanstack query providerの追加

--- apps/native/src/contexts/providers.tsx
+++ apps/native/src/contexts/providers.tsx
@@ -4,20 +4,23 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { SafeAreaListener } from "react-native-safe-area-context";
 import { Uniwind } from "uniwind";
+import { TanstackQueryClientProvider } from "./query-clinet-provider";
 import { SplashScreenController } from "./splash-screen-controller";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => (
-  <KeyboardProvider>
-    <SafeAreaListener
-      onChange={({ insets }) => {
-        Uniwind.updateInsets(insets);
-      }}
-    >
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <HeroUINativeProvider>
-          <SplashScreenController>{children}</SplashScreenController>
-        </HeroUINativeProvider>
-      </GestureHandlerRootView>
-    </SafeAreaListener>
-  </KeyboardProvider>
+  <TanstackQueryClientProvider>
+    <KeyboardProvider>
+      <SafeAreaListener
+        onChange={({ insets }) => {
+          Uniwind.updateInsets(insets);
+        }}
+      >
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <HeroUINativeProvider>
+            <SplashScreenController>{children}</SplashScreenController>
+          </HeroUINativeProvider>
+        </GestureHandlerRootView>
+      </SafeAreaListener>
+    </KeyboardProvider>
+  </TanstackQueryClientProvider>
 );
