f28b3c5661008f97723f3dba1729844bd22037f8
shouta0715 <kshouta0715@gmail.com>
2025-12-21 14:30:42 +0900

    feat: 記事知覧の取得を追加

--- apps/native/src/app/(app)/_layout.tsx
+++ apps/native/src/app/(app)/_layout.tsx
@@ -1,5 +1,25 @@
 import { Stack } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function AuthenticatedLayout() {
-  return <Stack />;
+  const insets = useSafeAreaInsets();
+
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerStyle: {
+            backgroundColor: "white",
+          },
+          headerShown: false,
+          contentStyle: {
+            backgroundColor: "white",
+            paddingBottom: insets.bottom,
+            paddingTop: insets.top,
+          },
+        }}
+      />
+    </Stack>
+  );
 }
